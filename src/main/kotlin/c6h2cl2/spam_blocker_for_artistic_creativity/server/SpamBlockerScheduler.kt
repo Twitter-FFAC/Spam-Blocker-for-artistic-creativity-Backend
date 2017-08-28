10a8222ebceb96b542be235aa8c6ab15c3d8436d
C6H2Cl2 <ma87kk26ml@gmail.com>
2017-08-29 08:47:56 +0900

    細かいバグ修正

--- src/main/kotlin/c6h2cl2/spam_blocker_for_artistic_creativity/server/SpamBlockerScheduler.kt
+++ src/main/kotlin/c6h2cl2/spam_blocker_for_artistic_creativity/server/SpamBlockerScheduler.kt
@@ -1,22 +1,18 @@
 package c6h2cl2.spam_blocker_for_artistic_creativity.server
 
-import c6h2cl2.spam_blocker_for_artistic_creativity.app.*
-
-import com.beust.klaxon.Parser
-import com.beust.klaxon.JsonObject
+import c6h2cl2.spam_blocker_for_artistic_creativity.app.runApp
 import com.beust.klaxon.JsonArray
-
-import java.util.Timer
-import java.util.TimerTask
-import java.io.BufferedReader
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
+import com.beust.klaxon.string
+import twitter4j.auth.AccessToken
 import java.io.File
-import java.io.InputStreamReader
-import java.net.URL
 import java.nio.file.Files
-import kotlin.streams.toList
+import java.util.*
+import kotlin.concurrent.schedule
 
 //4時間に1回実行する
-var scheduleTime = 14_400_000.toDouble()
+var scheduleTime = 14_400_000.toLong()
 
 fun runServer(args: Array<String>) {
     val timer = Timer()
@@ -39,10 +35,11 @@ private fun getUserSet(): Set<AccessToken> {
     }
     val json = String(Files.readAllBytes(path))
     val parser = Parser()
+    @Suppress("UNCHECKED_CAST")
     val array = parser.parse(json) as JsonArray<JsonObject>
     return array.filter {
         it.string("accessToken") != null && it.string("accessSecret") != null
     }.map {
         AccessToken(it.string("accessToken"), it.string("accessSecret"))
-    }
+    }.toSet()
 }
